905bc3fe58b0157e74b668dc3f156f9ab4ca1d06
Dobin <dobin@broken.ch>
2023-02-18 12:04:23 +0100

    refactor: better powershell script handling

--- make/powershell/powershell.py
+++ make/powershell/powershell.py
@@ -1,13 +1,14 @@
 import os, sys
 from base64 import b64encode
 from jinja2 import Template
+import logging
 
 # necessary to make it possible to execute this file from this directory
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from model import *
 
 # Notes:
-# Use powershell.exe argument '-noexit' for testing
+# Use "powershell.exe -noexit" for testing
 
 
 @PluginDecorator
@@ -21,12 +22,10 @@ def makePowershellDownloadAndExecuteMemPs1(url: str) -> AceStr:
     templateFile = 'download_exec_ps_enc_mem.ps1'
     with open('make/powershell/' + templateFile) as f:
         template = Template(f.read())
-    renderedHtml = template.render(
+    script = template.render(
         url=url
     )
-    renderedHtml = renderedHtml.replace('\r', '')
-    renderedHtml = renderedHtml.replace('\n', '')
-    return AceStr(renderedHtml)
+    return AceStr(script)
 
 
 @PluginDecorator
@@ -34,24 +33,38 @@ def makePowershellMessageBox() -> AceStr:
     templateFile = 'messagebox.ps1'
     with open('make/powershell/' + templateFile) as f:
         template = Template(f.read())
-    renderedHtml = template.render()
-    renderedHtml = renderedHtml.replace('\r', '')
-    renderedHtml = renderedHtml.replace('\n', '')
-    return AceStr(renderedHtml)
+    script = template.render()
+    return AceStr(script)
 
 
 @PluginDecorator
 def makePowershellCommand(input: str) -> AceStr:
     """For use with 'PowerShell.exe -Command {}'"""
-    ret = "{}".format(input)
+    ret = toPowershellLine(input)
     return AceStr(ret)
 
 
 @PluginDecorator
 def makePowershellEncodedCommand(input: str) -> AceStr:
     """For use with 'PowerShell.exe -EncodedCommand {}'"""
-    text = "{}".format(input)
+    text = toPowershellLine(input)
     # https://stackoverflow.com/questions/71642299/how-to-use-python-to-represent-the-powershell-tobase64string-function
     text = bytearray(text, 'utf-16-le')
     b64 = b64encode(text).decode() # bytearray to str
     return AceStr(b64)
+
+
+def toPowershellLine(input: AceStr) -> AceStr:
+    """Tries converting file-content with powershell code into a single line"""
+    # check if there's a ; at the end of each line
+    lines = input.splitlines()
+    for line in lines:
+        line = line.strip()
+        if not line.endswith(';'):
+            logging.warn("Powershell script without ';' at end of line: {}".format(line))
+
+    # remove all newlines
+    input = input.replace('\r\n', '')
+    input = input.replace('\n', '')
+
+    return input
